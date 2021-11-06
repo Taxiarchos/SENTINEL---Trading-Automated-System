b620fda6d044b91e82e0bd42e9590b043b8d4cce
رئيس الملائكة ميخائيل <CyberRonin0101@protonmail.com>
2021-11-07 00:55:39 +0100

    Update Intelligent_Investor.py

--- Main Algorithm and AI/Intelligent_Investor.py
+++ Main Algorithm and AI/Intelligent_Investor.py
@@ -1,16 +1,15 @@
-import Main_Algorithm import SENTINEL, SENTINEL_REDUX, SENTINEL_and_Indicators_output
+import Main_Algorithm import SENTINEL_REDUX, SENTINEL_and_Indicators_output
 from Efficiency_Testing import Test_Efficiency_for_Machine_Learning
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
 
 
 ticker_list = ['MSFT','MC','FB','TSLA','PYPL']
 OUTPUT = Test_Efficiency_for_Machine_Learning(ticker_list)
 
-for i in range(0,len(OUTPUT)):
-  for j in range(0,13):
-    if(OUTPUT[i][j] == 'SELL'):
-      OUTPUT[i][j] == 1
-    elif(OUTPUT[
 
-def Intelligent_Investor(OUTPUT):
 
 
