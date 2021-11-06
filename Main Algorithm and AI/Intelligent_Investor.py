5bab7792915933ee22e400004923d75be4837134
رئيس الملائكة ميخائيل <CyberRonin0101@protonmail.com>
2021-11-06 23:25:13 +0100

    Update Intelligent_Investor.py

--- Main Algorithm and AI/Intelligent_Investor.py
+++ Main Algorithm and AI/Intelligent_Investor.py
@@ -1,12 +1,15 @@
-import Main_Algorithm
-from 
+import Main_Algorithm import SENTINEL, SENTINEL_and_Indicators_output
+from Efficiency_Testing import Test_Efficiency_for_Machine_Learning
 
 
 ticker_list = ['MSFT','MC','FB','TSLA','PYPL']
-OUTPUT = SENTINEL_and_Indicators_output(ticker_list)
+OUTPUT = Test_Efficiency_for_Machine_Learning(ticker_list)
 
 for i in range(0,len(OUTPUT)):
-  for j in range(0,len(
+  for j in range(0,13):
+    if(OUTPUT[i][j] == 'SELL'):
+      OUTPUT[i][j] == 1
+    elif(OUTPUT[
 
 def Intelligent_Investor(OUTPUT):
 
