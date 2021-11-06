416f4d88209ce759d963444eb6125475220739b1
رئيس الملائكة ميخائيل <CyberRonin0101@protonmail.com>
2021-11-06 23:17:25 +0100

    Update Intelligent_Investor.py

--- Main Algorithm and AI/Intelligent_Investor.py
+++ Main Algorithm and AI/Intelligent_Investor.py
@@ -1,8 +1,12 @@
 import Main_Algorithm
+from 
 
 
 ticker_list = ['MSFT','MC','FB','TSLA','PYPL']
-OUTPUT = SENTINEL(ticker_list)
+OUTPUT = SENTINEL_and_Indicators_output(ticker_list)
+
+for i in range(0,len(OUTPUT)):
+  for j in range(0,len(
 
 def Intelligent_Investor(OUTPUT):
 
