dc3b1a7e31df8d81ba8d911d22b67cd4c2b3fa77
رئيس الملائكة ميخائيل <CyberRonin0101@protonmail.com>
2021-11-14 05:29:10 +0100

    Update Stock_Price_Info.py

--- DATA MINING/STOCK DATA/Stock_Price_Info.py
+++ DATA MINING/STOCK DATA/Stock_Price_Info.py
@@ -1,18 +1,13 @@
+import yfinance as yf
+
+
 def Stock_List_Gen(ticker_list):
     historical_price_list = []
-
-    for stock_name in ticker_list: 
-        msft = yf.Ticker(stock_name)
-        info=[["Info", msft.info],["historical market data", msft.history(period="max")],["Actions", msft.actions],["Dividends", msft.dividends],["Splits", msft.splits],["Financials", msft.financials],
-            ["Quarterly financials", msft.quarterly_financials],["Major holders", msft.major_holders],["Institutional holders", msft.institutional_holders],["Balance sheet", msft.balance_sheet],
-            ["Quarterly Balance sheet", msft.quarterly_balance_sheet], ["Cash Flow", msft.cashflow], ["Quarterly cash flow", msft.quarterly_cashflow], ["Earnings", msft.earnings],
-            ["Quarterly earnings", msft.quarterly_earnings], ["Sustainability", msft.sustainability],  ["Recommendations", msft.recommendations], ["Calendar of next events", msft.calendar],
-            ["Options expirations", msft.options]]
-                
-        stock_price=pd.DataFrame(info[1][1])
-        stock_price.reset_index(inplace=True)
-
-        stock_price_array = stock_price["Close"]
-        historical_price_list.append(stock_price_array)
-
+    
+    for stock_name in ticker_list:
+        stock_data = yf.download(stock_name)
+        stock_price = stock_data["Close"]
+        
+        historical_price_list.append(stock_price)
+        
     return historical_price_list
