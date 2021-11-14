52471a30a4e2111006c2c5cf27afadf1c8a46d8b
رئيس الملائكة ميخائيل <CyberRonin0101@protonmail.com>
2021-11-14 05:41:32 +0100

    Update Stock_Price_Info.py

--- DATA MINING/STOCK DATA/Stock_Price_Info.py
+++ DATA MINING/STOCK DATA/Stock_Price_Info.py
@@ -4,7 +4,9 @@ import yfinance as yf
 def Stock_List_Gen(ticker_list):
     historical_price_list = []
     
-    for stock_name in ticker_list:                           
+    for stock_name in ticker_list:       
+        #start_date=01-01-2000
+        #end_date=08-01-2000
         stock_data = yf.download(stock_name, interval="1d")  #The interval can be "1m" for 1 minute, "1h" for 1 hour, "1d" for 1 day, and so on.
         stock_price = stock_data["Close"]                    #IF YOU DESIRE (FOR EXAMPLE "1m" AS INTERVAL, REMEMBER TO CREATE TWO VARIABLES
                                                              #start_date and end_date, the datatype is timedate (DD-MM-YY) and the max timeframe
