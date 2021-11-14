c4c07ec6f3a27998bf5b7e47526f0ef95288346d
رئيس الملائكة ميخائيل <CyberRonin0101@protonmail.com>
2021-11-14 05:35:16 +0100

    Update Stock_Price_Info.py

--- DATA MINING/STOCK DATA/Stock_Price_Info.py
+++ DATA MINING/STOCK DATA/Stock_Price_Info.py
@@ -10,4 +10,5 @@ def Stock_List_Gen(ticker_list):
         
         historical_price_list.append(stock_price)
         
-    return historical_price_list
+    return historical_price_list               #For example Stock_List_Gen(['TSLA']) RICORDA CHE L'ARGOMENTO ticker_list E' UNA
+                                               #LISTA! QUINDI ANCHE SE C'E' UN SOLO VALORE, QUESTO VA TRA PARENTESI QUADRE.
