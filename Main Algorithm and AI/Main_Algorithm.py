6322265cd6f107ff4b094944fab908e7b0d77d2b
رئيس الملائكة ميخائيل <CyberRonin0101@protonmail.com>
2021-11-14 07:09:43 +0100

    Update Main_Algorithm.py

--- Main Algorithm and AI/Main_Algorithm.py
+++ Main Algorithm and AI/Main_Algorithm.py
@@ -8,6 +8,9 @@ from Technical_Indicators import Bollinger_Signal
 from Technical_Indicators import RSI_Signal
 from Technical_Indicators import CMO_Signal
 from SENTINEL/'DATA MINING'/'STOCK DATA'/Stock_Price_Info import Stock_List_Gen
+from SENTINEL/'DATA MINING'/'FOREX DATA'/
+from SENTINEL/'DATA MINING'/'CRYPTOCURRENCIES DATA'/
+from SENTINEL/'DATA MINING'/'FUTURES AND COMMODITIES DATA'/
 
 
 
