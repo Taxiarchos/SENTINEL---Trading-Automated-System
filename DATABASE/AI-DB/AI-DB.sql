94e2dc2122f15b73ec6080c18a3db00412fd12b1
رئيس الملائكة ميخائيل <CyberRonin0101@protonmail.com>
2021-11-27 02:21:29 +0100

    Update AI-DB.sql

--- DATABASE/AI-DB/AI-DB.sql
+++ DATABASE/AI-DB/AI-DB.sql
@@ -1,1 +1,15 @@
-
+CREATE TABLE AI_Dataset (                      -- 0 is buy, 1 is sell
+   fermat_signal_opening INTEGER(1) NOT NULL,
+   bollinger_opening INTEGER(1) NOT NULL,  
+   rsi_opening INTEGER(1) NOT NULL,
+   cmo_opening INTEGER(1) NOT NULL,
+   fermat_signal_closing INTEGER(1) NOT NULL,
+   bollinger_closing INTEGER(1) NOT NULL,
+   rsi_closing INTEGER(1) NOT NULL,
+   cmo_closing INTEGER(1) NOT NULL,
+   opening_price INTEGER(1) NOT NULL,
+   closing_price INTEGER(1) NOT NULL,
+   final_signal_opening INTEGER(1) NOT NULL,
+   final_signal_closing INTEGER(1) NOT NULL,
+   result INTEGER(1) NOT NULL                   -- 0 is positive result, 1 is neutral or negative
+   );
