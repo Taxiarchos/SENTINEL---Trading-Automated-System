92427e2a5f563cfedb5af211104f9ca23ad6c91f
رئيس الملائكة ميخائيل <CyberRonin0101@protonmail.com>
2021-12-22 15:47:55 +0100

    Update __init__.py

--- Author/__init__.py
+++ Author/__init__.py
@@ -1,1 +1,7 @@
+name = "SENTINEL - Trading Automated System"
+__version__ = "0.0.1"
+__author__ = "Taxiarchos"
 
+print("Thank you for using ", name, "\n")
+print("version: ", __version__, "\n")
+print("by ", __author__, "\n")
